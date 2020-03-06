75aa110c1cb395757f507caa5d61f1bf237346b1
V1tos <victor.palamarchuk@gmail.com>
2020-03-06 11:46:16 +0200

    Array completed

--- Task 1 - Arrays/Task 1 - Arrays/Program.cs
+++ Task 1 - Arrays/Task 1 - Arrays/Program.cs
@@ -23,104 +23,151 @@ namespace Task_1___Arrays
 
         static void Main(string[] args)
         {
+            //#region
+            //int[,] matrix = new int[5, 5];
+            //Random rand = new Random();
+            //for (int i = 0; i < matrix.GetUpperBound(0)+1; i++)
+            //{
+            //    for (int j = 0; j < matrix.GetUpperBound(1)+1; j++)
+            //    {
+            //        matrix[i, j] = rand.Next(-100,100);
 
-            int[,] matrix = new int[5, 5];
-            Random rand = new Random();
-            for (int i = 0; i < matrix.GetUpperBound(0)+1; i++)
-            {
-                for (int j = 0; j < matrix.GetUpperBound(1)+1; j++)
-                {
-                    matrix[i, j] = rand.Next(-100,100);
-
-                }
-            }
-
-            for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
-                {
-                    Console.Write("[{0}]", matrix[i, j]);
-                }
-                Console.WriteLine();
-            }
-
-
-            int min, max;
-            int minI=0, minJ=0;
-            int maxI=0, maxJ=0;
-          
-
-            max = matrix[0, 0];
-            for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
-                {
-                    if (matrix[i, j] > max)
-                    {
-                        max = matrix[i, j];
-                        maxI = i;
-                        maxJ = j;
-                    }
-                                      
-                }
-            }
-
-
-            min = matrix[0, 0];
-            for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
-                {
-                    if (matrix[i, j] < min)
-                    {
-                        min = matrix[i, j];
-                        minI = i;
-                        minJ = j;
-                    }
-                        
-                }
-            }
-
-            int sum=0;
-
-            int maxQueue = maxI * (matrix.GetUpperBound(0) + 1) + maxJ;
-            int minQueue = minI * (matrix.GetUpperBound(0) + 1) + minJ;
-
-            Console.WriteLine(minQueue);
-            Console.WriteLine(maxQueue);
-
-            if (maxQueue>minQueue)
-            {
-                for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
-                {
-                    for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
-                    {
-                        if (i >= minI && j>=minJ&&i<=maxI&&j<=maxJ)
-                        {
-                            sum += matrix[i, j];
-
-                        }
-
-                    }
-                }
-            }
+            //    }
+            //}
 
             //for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
             //{
             //    for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
             //    {
-            //        if (i >= minI && matrix[i, j] <= max)
+            //        Console.Write("[{0}]", matrix[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int min, max;
+            //int minI=0, minJ=0;
+            //int maxI=0, maxJ=0;
+          
+
+            //max = matrix[0, 0];
+            //for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+            //{
+            //    for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
+            //    {
+            //        if (matrix[i, j] > max)
             //        {
-            //            sum += matrix[i, j];
-
+            //            max = matrix[i, j];
+            //            maxI = i;
+            //            maxJ = j;
             //        }
-
+                                      
             //    }
             //}
 
-            Console.WriteLine("Min[i] = {0},Min[j] = {1}\nMax[i] = {2},Max[j] = {3}",minI, minJ,maxI,maxJ);
-            Console.WriteLine("Max: {0}\nMin: {1}\nSum: {2}", max, min,sum);
-            
+
+            //min = matrix[0, 0];
+            //for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+            //{
+            //    for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
+            //    {
+            //        if (matrix[i, j] < min)
+            //        {
+            //            min = matrix[i, j];
+            //            minI = i;
+            //            minJ = j;
+            //        }
+                        
+            //    }
+            //}
+
+            //int sum=0;
+
+            //int maxQueue = maxI * (matrix.GetUpperBound(0) + 1) + maxJ;
+            //int minQueue = minI * (matrix.GetUpperBound(0) + 1) + minJ;
+            //int counter = 0;
+
+            //Console.WriteLine($"Min queue: {minQueue}");
+            //Console.WriteLine($"Max queue: {maxQueue}");
+
+            //if (maxQueue>minQueue)
+            //{
+            //    for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+            //    {
+            //        for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
+            //        {
+            //            if (counter>minQueue && counter<maxQueue)
+            //            {
+            //                sum += matrix[i, j];
+            //            }
+            //            counter++;
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+            //    {
+            //        for (int j = 0; j < matrix.GetUpperBound(1) + 1; j++)
+            //        {
+            //            if (counter > maxQueue && counter < minQueue)
+            //            {
+            //                sum += matrix[i, j];
+            //            }
+            //            counter++;
+            //        }
+            //    }
+            //}
+
+           
+            //Console.WriteLine("Min[i] = {0},Min[j] = {1}\nMax[i] = {2},Max[j] = {3}",minI, minJ,maxI,maxJ);
+            //Console.WriteLine("Max: {0}\nMin: {1}\nSum: {2}", max, min,sum);
+
+            //#endregion
+
+            #region
+            int[] arr = new int[30];
+            int counter = 0, tmpcounter = 0;
+            int first = 0, last = 0;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(50);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+
+                    tmpcounter++;
+                }
+                else
+                {
+                    if (counter < tmpcounter)
+                    {
+                        first = i - tmpcounter;
+                        last = first + tmpcounter;
+                        counter = tmpcounter;
+                    }
+                    tmpcounter = 0;
+                }
+
+            }
+
+            Console.WriteLine($"Count growing digits: {counter}\nFirst index: {first}\nLast index: {last}");
+
+
+            for (int i = first; i <= last; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            #endregion
 
         }
     }
